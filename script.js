18929c9f73d3b61f6807f2dacc7ec4155d59c905
KEN-2000l <79527273+KEN-2000l@users.noreply.github.com>
2021-03-15 14:32:27 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,103 @@
 // ==UserScript==
 // @name         Education Perfected (Auto-Answer) DEV
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Shitty script to F**k Education Perfect Tasks (and learn basic js)
+// @version      0.2
+// @description  Shitty script to auto-answer Education Perfect Tasks (and learn basic js)
 // @author       KEN_2000
 // @match        *://www.educationperfect.com/app/*
 // @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // ==/UserScript==
 
 (function() {
     'use strict';
+    var TOGGLE = false;
+    var LOOP, fullDict;
 
-    console.log('Education Perfected by KEN Loading')
+    console.log('Education Perfected by KEN Loading');
 
     function wordlistBase() {
         var elements = document.getElementsByClassName('baseLanguage');
-        var words = []
+        var words = [];
         for (let i = 0; i < elements.length; i++) {
-        words.push(elements[i].innerText)
-        }
-        console.log(words);
-        return words
-    }
+            words.push(elements[i].innerText);
+        };
+        return words;
+    };
 
     function wordlistTarget() {
         var elements = document.getElementsByClassName('targetLanguage');
-        var words = []
+        var words = [];
         for (let i = 0; i < elements.length; i++) {
-        words.push(elements[i].innerText)
-        }
-        console.log(words);
-        return words
-    }
+            words.push(elements[i].innerText);
+        };
+        return words;
+    };
 
     function mergeLists(l1, l2) {
-        var mergedWords = {}
+        var mergedWords = {};
         for (let i = 0; i < l1.length; i++) {
-            var w1 = l1[i].split('; ').slice(-1)
-            var w2 = l2[i].split('; ').slice(-1)
+            var w1 = l1[i].split('; ').slice(-1);
+            var w2 = l2[i].split('; ').slice(-1);
             mergedWords[w2] = w1;
             mergedWords[w1] = w2;
-        }
-        return mergedWords
-    }
+        };
+        return mergedWords;
+    };
+
+    function nextQuestion() {
+        if (TOGGLE = true) {setTimeout(answerQuestion, 500)};
+    };
+
+    function answerQuestion() {
+        try {
+            var question = document.getElementById('question-text').childNodes[0].innerText;
+
+            if (question != undefined) {
+                console.log('QUESTION: ' + question);
+                var answer = String(fullDict[question]);
+
+                if (answer != undefined && answer != 'undefined') {
+                    console.log('ANSWER: ' + answer);
+                    var answerBox = document.querySelectorAll('#answer-text')[1];
+                    // Find out how to input answer?!?!?! It apparently somehow always resets the textbox value if you do x.value = asdf
+                    answerBox.value = answer;
+                    document.querySelector('#submit-button').click();
+                    nextQuestion();
+                } else {
+                    console.log('No Answer Found');
+                    nextQuestion();
+                };
+
+            } else {
+                console.log('No Question Found');
+                nextQuestion();
+            };
+        } catch {
+            TOGGLE = false
+            alert('Auto-Answer Stopped')
+        };
+    };
 
     document.addEventListener("keydown", (event) => {
-        if(event.shiftKey && event.keyCode === 84) {
-            var fullDict = mergeLists(wordlistBase(), wordlistTarget());
-            console.log(fullDict)
-        }
+        if (event.altKey && event.keyCode === 82) {
+            fullDict = mergeLists(wordlistBase(), wordlistTarget());
+            console.log(fullDict);
+            alert('Word List Refreshed');
+        };
     });
 
+    document.addEventListener("keydown", (event) => {
+        if (event.altKey && event.keyCode === 65) {
+            if (TOGGLE === false) {
+                alert('Starting Auto-Answer');
+                TOGGLE = true;
+                answerQuestion();
+            } else if (TOGGLE === true) {
+                alert('Stopping Auto-Answer');
+                TOGGLE = false;
+            };
+        };
+    });
 })();
+
