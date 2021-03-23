65f39e9592a7f97cc75ed694f234bf11848da583
KEN-2000l <79527273+KEN-2000l@users.noreply.github.com>
2021-03-23 17:58:02 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 // @name         Education Perfected (Auto-Answer) DEV
 // @namespace    http://tampermonkey.net/
 // @version      0.2.3
-// @updateURL    https://github.com/KEN-2000l/EducationPerfected/blob/main/script.js
-// @downloadURL  https://github.com/KEN-2000l/EducationPerfected/blob/main/script.js
+// @updateURL    https://raw.githubusercontent.com/KEN-2000l/EducationPerfected/main/script.js
+// @downloadURL  https://raw.githubusercontent.com/KEN-2000l/EducationPerfected/main/script.js
 // @description  Shitty script to auto-answer Education Perfect Tasks (and learn basic js)
 // @author       KEN_2000
 // @match        *://www.educationperfect.com/app/*
