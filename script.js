b4a29709b219ef52d7e0855e5b3aa20287ef1cb8
Garv Shah <64292655+garv-shah@users.noreply.github.com>
2021-06-22 21:42:00 +1000

    Added auto-answer functionality
    I left the original functionality intact but added the option to use ALT + S to actually answer the questions in a fully automated fashion. Code could be compressed in future if depreciated functionality was removed.

--- script.js
+++ script.js
@@ -1,72 +1,91 @@
 (function() {
     'use strict';
-    var TOGGLE = false;
-    var fullDict;
+    let TOGGLE = false;
+    let fullDict;
+    let globalTime = 0;
 
     console.log('Education Perfected by Garv and KEN');
 
     function wordlistBase() {
-        var elements = document.getElementsByClassName('baseLanguage');
-        var words = [];
+        const elements = document.getElementsByClassName('baseLanguage');
+        let words = [];
         for (let i = 0; i < elements.length; i++) {
             words.push(elements[i].innerText);
-        };
+        }
         return words;
-    };
+    }
 
     function wordlistTarget() {
-        var elements = document.getElementsByClassName('targetLanguage');
-        var words = [];
+        const elements = document.getElementsByClassName('targetLanguage');
+        let words = [];
         for (let i = 0; i < elements.length; i++) {
             words.push(elements[i].innerText);
-        };
+        }
         return words;
-    };
+    }
 
     function mergeLists(l1, l2) {
-        var mergedWords = {};
+        const mergedWords = {};
         for (let i = 0; i < l1.length; i++) {
-            var w1 = l1[i].replace(/ *\([^)]*\) */g, "").split('; ').slice(0, 1);
-            var w2 = l2[i].replace(/ *\([^)]*\) */g, "").split('; ').slice(0, 1);
+            const w1 = l1[i].replace(/ *\([^)]*\) */g, "").split('; ').slice(0, 1);
+            const w2 = l2[i].replace(/ *\([^)]*\) */g, "").split('; ').slice(0, 1);
             mergedWords[w2] = w1;
             mergedWords[w1] = w2;
-        };
+        }
         return mergedWords;
-    };
+    }
 
     function nextQuestion() {
-        if (TOGGLE = true) {
+        if (TOGGLE === true) {
             setTimeout(answerQuestion, 100);
-        };
-    };
+        }
+    }
+
+    let doAnswers = () => {
+        if (TOGGLE === true) {
+            let question = document.querySelectorAll('#question-text')[0].innerText;
+
+            if (question !== undefined) {
+                question = question.replace(/ *\([^)]*\) */g, "").split(', ').slice(0, 1);
+                const answer = String(fullDict[question]);
+
+                document.getElementsByTagName('button')[7].click();
+                document.getElementsByTagName("input")[0].value = answer
+                setTimeout(doAnswers, globalTime);
+            }
+        } else {
+            TOGGLE = false
+            console.log("the program has been terminated");
+        }
+    }
 
     function answerQuestion() {
         try {
-            var question = document.querySelectorAll('#question-text')[0].innerText;
+            let question = document.querySelectorAll('#question-text')[0].innerText;
 
-            if (question != undefined) {
+            if (question !== undefined) {
                 question = question.replace(/ *\([^)]*\) */g, "").split(', ').slice(0, 1);
-                var answer = String(fullDict[question]);
+                const answer = String(fullDict[question]);
 
                 navigator.clipboard.writeText(answer);
 
             } else {
                 console.log('No Question Found');
-            };
+            }
             nextQuestion();
         } catch {
             TOGGLE = false;
             console.log('Error');
             alert('Auto-Answer Stopped');
-        };
-    };
+        }
+    }
 
     document.addEventListener("keydown", (event) => {
         if (event.altKey && event.keyCode === 82) {
             fullDict = mergeLists(wordlistBase(), wordlistTarget());
             console.log(fullDict);
             alert('Word List Refreshed');
-        };
+        }
     });
 
     document.addEventListener("keydown", (event) => {
@@ -78,7 +97,21 @@
             } else if (TOGGLE === true) {
                 alert('Stopping Auto-Answer');
                 TOGGLE = false;
-            };
-        };
+            }
+        }
+    });
+
+    document.addEventListener("keydown", (event) => {
+        if (event.altKey && event.keyCode === 83) {
+            if (TOGGLE === false) {
+                console.log('Starting Auto-Answer (fully automated)');
+                TOGGLE = true;
+                globalTime = prompt("How fast would you like to answer the questions (in milliseconds)")
+                doAnswers();
+            } else if (TOGGLE === true) {
+                alert('Stopping Auto-Answer (fully automated)');
+                TOGGLE = false;
+            }
+        }
     });
 })();
