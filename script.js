c665880794e4542c3e94679b28bb600a0e3bb897
Garv Shah <64292655+garv-shah@users.noreply.github.com>
2021-06-24 11:09:22 +1000

    switch from explanation-button to submit-button
    for some reason the button being pressed on submit wasn't the submit button, it was the button asking for help. dunno how that got through but it's fixed now ig

--- script.js
+++ script.js
@@ -11,26 +11,26 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+    "use strict";
     let TOGGLE = false;
     let fullDict;
     let loopInterval = 100;
 
-    console.log('Education Perfected by Garv and KEN Loading');
+    console.log("Education Perfected by Garv and KEN Loading");
 
     function wordList(className) {
         const elements = document.getElementsByClassName(className);
         let words = [];
         for (let i = 0; i < elements.length; i++) {
-            let word = String(elements[i].innerText.replace(/ *\([^)]*\) */g, "").split('; ').slice(0, 1));
-            word = String(word.split(', ').slice(0, 1));
+            let word = String(elements[i].innerText.replace(/ *\([^)]*\) */g, "").split("; ").slice(0, 1));
+            word = String(word.split(", ").slice(0, 1));
             words.push(word);
         }
         return words;
     }
 
     function mergeLists(l1, l2) {
-        var merged = {};
+        let merged = {};
         for (let i = 0; i < l1.length; i++) {
             merged[l2[i]] = l1[i];
             merged[l1[i]] = l2[i];
@@ -40,7 +40,7 @@
 
     function cutString(string) {
         string = String(string.replace(/ *\([^)]*\) */g, "").split("; ").slice(0, 1));
-        string = String(string.split(', ').slice(0, 1));
+        string = String(string.split(", ").slice(0, 1));
         return string
     }
 
@@ -57,31 +57,31 @@
             setTimeout(function(){ fullDict[cutString(document.querySelector("#question-field").innerText)] = document.querySelector("#correct-answer-field").innerText; }, 1500);
             setTimeout(function(){ document.querySelector("#continue-button").click(); }, 500);
         } else {
-            document.getElementById("explanation-button").click();
-            document.getElementsByTagName('input')[0].value = answer;
+            document.getElementById("#submit-button").click();
+            document.getElementsByTagName("input")[0].value = answer;
         }
     }
 
     function answerLoop(answerFunc) {
         try {
-            let question = document.querySelectorAll('#question-text')[0].innerText;
+            let question = document.querySelectorAll("#question-text")[0].innerText;
 
-            if (question != undefined) {
-                question = question.replace(/ *\([^)]*\) */g, '').split(', ').slice(0, 1);
+            if (question !== undefined) {
+                question = question.replace(/ *\([^)]*\) */g, "").split(", ").slice(0, 1);
                 let answer = fullDict[question];
 
                 answerFunc(answer)
 
             } else {
-                console.log('No Question Found');
+                console.log("No Question Found");
             }
             if (TOGGLE === true) {
                 setTimeout(function(){answerLoop(answerFunc)}, loopInterval);
             }
         } catch {
             TOGGLE = false;
-            console.log('An Error has occured.');
-            alert('Error, Auto-Answer Stopped.');
+            console.log("An Error has occured.");
+            alert("Error, Auto-Answer Stopped.");
         }
     }
 
@@ -89,18 +89,18 @@
         if (event.altKey && event.keyCode === 82) {
             fullDict = mergeLists(wordList("targetLanguage"), wordList("baseLanguage"));
             console.log(fullDict);
-            alert('Word List Refreshed');
+            alert("Word List Refreshed");
         }
     })
 
     document.addEventListener("keydown", (event) => {
         if (event.altKey && event.keyCode === 65) {
             if (TOGGLE === false) {
-                alert('Starting Semi-Auto-Answer');
+                alert("Starting Semi-Auto-Answer");
                 TOGGLE = true;
                 answerLoop(copyAnswer);
             } else if (TOGGLE === true) {
-                alert('Stopping Semi-Auto-Answer');
+                alert("Stopping Semi-Auto-Answer");
                 TOGGLE = false;
             }
         }
@@ -110,11 +110,11 @@
         if (event.altKey && event.keyCode === 83) {
             if (TOGGLE === false) {
                 loopInterval = prompt("How fast would you like to answer the questions (in milliseconds)")
-                alert('Starting Fully-Auto-Answer');
+                alert("Starting Fully-Auto-Answer");
                 TOGGLE = true;
                 answerLoop(submitAnswer);
             } else if (TOGGLE === true) {
-                alert('Stopping Fully-Auto-Answer');
+                alert("Stopping Fully-Auto-Answer");
                 TOGGLE = false;
             }
         }
