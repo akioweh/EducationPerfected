cd303bb3a6869456bef24425d7aa5d22a0839411
Garv Shah <64292655+garv-shah@users.noreply.github.com>
2021-08-23 09:08:02 +1000

    Cleaned Codebase and Fixed Bug
    Made the codebase a bit more concise, added comments and stuff, and I also fixed the bug i described before

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Education Perfected (Auto-Answer)
 // @namespace    mailto:gshah.6110@gmail.com
-// @version      0.5.0
+// @version      0.5.1
 // @updateURL    https://raw.githubusercontent.com/KEN-2000l/EducationPerfected/main/script.js
 // @downloadURL  https://raw.githubusercontent.com/KEN-2000l/EducationPerfected/main/script.js
 // @description  Basic Script to auto-answer Education Perfect Tasks
@@ -12,14 +12,25 @@
 
 (function() {
     "use strict";
+
+    // List of global variables
     let semiTOGGLE = false;
     let autoTOGGLE = false;
     let fullDict;
-    let loopInterval = 100;
+    let loopInterval = 10;
     let answerSub = ""
 
-    console.log("Education Perfected by Garv and KEN Loading");
+    console.log("Education Perfected by Garv and KEN Loaded");
 
+    // These are all internal functions which are usd quite frequently
+
+    // Sets both toggles
+    function setToggle(semiState, autoState) {
+        semiTOGGLE = semiState;
+        autoTOGGLE = autoState;
+    }
+
+    // Gets the list of words from the specified class. Used solely to declare fullDict
     function wordList(className) {
         const elements = document.getElementsByClassName(className);
         let words = [];
@@ -31,6 +42,7 @@
         return words;
     }
 
+    // Merges to lists
     function mergeLists(l1, l2) {
         let merged = {};
         for (let i = 0; i < l1.length; i++) {
@@ -40,14 +52,14 @@
         return merged;
     }
 
+    // Cuts a string to be of the specified format so that they are all consistent
     function cutString(string) {
         string = String(string.replace(/ *\([^)]*\) */g, "").split("; ").slice(0, 1));
         string = String(string.split(", ").slice(0, 1));
         return string
     }
 
-    function copyAnswer(answer) {}
-
+    // Submits the answer
     function submitAnswer(answer) {
         if (document.querySelector("#question-field") != null) {
             setTimeout(function(){ fullDict[cutString(document.querySelector("#question-field").innerText)] = document.querySelector("#correct-answer-field").innerText.split(" | ")[0]; }, 1500);
@@ -58,7 +70,9 @@
         }
     }
 
-    function answerLoop(answerFunc) {
+    // The main loop, which runs as specified in the loopInterval
+    function answerLoop() {
+        // Presuming there's a question, it grabs the relevant answer from fullDict
         try {
             let question = document.querySelectorAll("#question-text")[0].innerText;
 
@@ -66,64 +80,64 @@
                 question = question.replace(/ *\([^)]*\) */g, "").split(", ").slice(0, 1);
                 answerSub = fullDict[question];
 
-                answerFunc(answerSub)
-
+                if (autoTOGGLE === true) {
+                    // If we are in fully-automatic, the script automatically submits the answer
+                    submitAnswer(answerSub)
+                }
             } else {
                 console.log("No Question Found");
             }
+
+            // Calls the loop again
             if ((semiTOGGLE === true) || (autoTOGGLE === true)) {
-                setTimeout(function(){answerLoop(answerFunc)}, loopInterval);
+                setTimeout(function(){answerLoop()}, loopInterval);
             }
         } catch (err) {
-            semiTOGGLE = false;
-            autoTOGGLE = false;
+            setToggle(false, false)
             console.log("An Error has occurred.");
             console.log(err);
             alert("Error, Auto-Answer Stopped.");
         }
     }
 
+    // This adds an event listener which reacts appropriately based on the keyboard keys being clicked
     document.addEventListener("keydown", (event) => {
-        if (event.altKey && event.keyCode === 82) {
-            fullDict = mergeLists(wordList("targetLanguage"), wordList("baseLanguage"));
-            console.log(fullDict);
-            alert("Word List Refreshed");
-        }
-    })
-
-    document.addEventListener("keydown", (event) => {
-        if (event.altKey && event.keyCode === 65) {
-            if (semiTOGGLE === false) {
-                alert("Starting Semi-Auto-Answer");
-                semiTOGGLE = true;
-                answerLoop(copyAnswer);
-            } else if (semiTOGGLE === true) {
-                alert("Stopping Semi-Auto-Answer");
-                semiTOGGLE = false;
+        // Opt/Alt + S
+        if ((event.altKey && event.keyCode === 83) || (event.altKey && event.key === 's')) {
+            if (autoTOGGLE === false) {
+                loopInterval = parseInt(prompt("How fast would you like to answer the questions (in milliseconds)"))
+                alert("Starting Fully-Auto-Answer");
+                setToggle(false, true);
+                answerLoop();
+            } else if (autoTOGGLE === true) {
+                alert("Stopping Fully-Auto-Answer");
+                loopInterval = 10;
+                setToggle(false, false);
             }
         }
-    });
-
-    document.addEventListener("keydown", (event) => {
-        if ((event.metaKey && semiTOGGLE === true) || (event.ctrlKey && semiTOGGLE === true)) {
+        // Cmd/Ctrl
+        else if ((event.metaKey && semiTOGGLE === true) || (event.ctrlKey && semiTOGGLE === true)) {
             navigator.clipboard.writeText(answerSub).then(function(arr) {}, function(arr) {
                 console.log(`Failed to Copy Answer: ${arr}`)
             });
         }
-    });
-
-    document.addEventListener("keydown", (event) => {
-        if (event.altKey && event.keyCode === 83) {
-            if (autoTOGGLE === false) {
-                loopInterval = parseInt(prompt("How fast would you like to answer the questions (in milliseconds)"))
-                alert("Starting Fully-Auto-Answer");
-                autoTOGGLE = true;
-                answerLoop(submitAnswer);
-            } else if (autoTOGGLE === true) {
-                alert("Stopping Fully-Auto-Answer");
-                autoTOGGLE = false;
+        // Opt/Alt + A
+        else if ((event.altKey && event.keyCode === 65) || (event.altKey && event.key === 'a')) {
+            if (semiTOGGLE === false) {
+                alert("Starting Semi-Auto-Answer");
+                loopInterval = 10;
+                setToggle(true, false);
+                answerLoop();
+            } else if (semiTOGGLE === true) {
+                alert("Stopping Semi-Auto-Answer");
+                setToggle(false, false);
             }
         }
+        // Opt/Alt + R
+        else if ((event.altKey && event.keyCode === 82) || (event.altKey && event.key === 'r')) {
+            fullDict = mergeLists(wordList("targetLanguage"), wordList("baseLanguage"));
+            console.log(fullDict);
+            alert("Word List Refreshed");
+        }
     });
-
 })();
