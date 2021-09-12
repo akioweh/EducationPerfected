5301ec1f95d4b6ef28bf72fd292b22fac4ec0a11
KEN-2000l <79527273+KEN-2000l@users.noreply.github.com>
2021-09-12 09:55:36 +0400

    Full speed auto answer; improved self-learning
    Clean max-speed code
    (Sorry I just didn't include customizable speed because I think it is unnecessary, and I'm lazy)
    Rewrote question parsing and answer matching to resolve edge cases
    Changed how self-learning works so it can always correct itself
    DELETES modals to skip the wrong answer screen even faster
    
    P.S. There is still an edge case when two different answers have the same question, where education perfect will itself add `(starts with "letter")` to the question. These questions will be wrong the first time but will get corrected. It is possible to get it right the first time by parsing the starting letter from the question but I'm lazy again.

--- script.js
+++ script.js
@@ -1,177 +1,152 @@
 // ==UserScript==
 // @name         Education Perfected (Auto-Answer)
-// @namespace    mailto:gshah.6110@gmail.com
-// @version      1.0.2
+// @namespace    http://tampermonkey.net/
+// @version      1.1.0
 // @updateURL    https://raw.githubusercontent.com/KEN-2000l/EducationPerfected/main/script.js
 // @downloadURL  https://raw.githubusercontent.com/KEN-2000l/EducationPerfected/main/script.js
-// @description  Basic Script to auto-answer Education Perfect Tasks
+// @description  Auto-answer Education Perfect Tasks at HIGH Speeds
 // @author       KEN_2000, Garv
 // @match        *://www.educationperfect.com/app/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(async function () {
     "use strict";
-
-    // List of global variables
     let semiTOGGLE = false;
     let autoTOGGLE = false;
     let fullDict = {};
-    let failCount = 0;
-    let prevQuestion = null;
-    let loopInterval = 10;
-    let answerSub = ""
+    let cutDict = {};
+    let msg = "";
 
-    console.log("Education Perfected by Garv and KEN Loaded");
-
-    // These are all internal functions which are usd quite frequently
-
-    // Sets both toggles
-    function setToggle(semiState, autoState) {
-        semiTOGGLE = semiState;
-        autoTOGGLE = autoState;
-    }
-
-    // Simple sleep function used for **speed**
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    // Gets the list of words from the specified class. Used solely to declare fullDict
+    function cutString(string) {
+        string = String(string.replace(/ *\([^)]*\) */g, "").split("; ")[0]);
+        string = String(string.split(", ")[0]);
+        return string;
+    }
+
     function wordList(className) {
-        const elements = document.getElementsByClassName(className);
         let words = [];
-        for (let i = 0; i < elements.length; i++) {
-            let word = String(elements[i].innerText.replace(/ *\([^)]*\) */g, "").split("; ").slice(0, 1));
-            word = String(word.split(", ").slice(0, 1));
-            words.push(word);
-        }
+        document.querySelectorAll("div." + className).forEach(i => words.push(i.innerText));
         return words;
     }
 
-    // Merges to lists
     function mergeLists(l1, l2) {
-        let merged = {};
         for (let i = 0; i < l1.length; i++) {
-            merged[l2[i]] = l1[i];
-            merged[l1[i]] = l2[i];
+            fullDict[l2[i]] = cutString(l1[i]);
+            fullDict[l1[i]] = cutString(l2[i]);
+            cutDict[cutString(l2[i])] = cutString(l1[i]);
+            cutDict[cutString(l1[i])] = cutString(l2[i]);
         }
-        return merged;
     }
 
-    // Cuts a string to be of the specified format so that they are all consistent
-    function cutString(string) {
-        string = String(string.replace(/ *\([^)]*\) */g, "").split("; ").slice(0, 1));
-        string = String(string.split(", ").slice(0, 1));
-        return string
+    async function submitButton() {
+        let explanation = document.querySelector("button#explanation-button");
+        if (explanation) return explanation;
+        console.log("didn't find hax button");
+        return document.querySelector("button.submit-button");
     }
 
-    // Submits the answer :D
-    function submitAnswer(answer) {
-        console.log('answering ' + answer);
-        document.querySelector("#explanation-button").click();
-        document.getElementsByTagName("input")[0].value = answer;
+    function deleteModals() {
+        document.querySelectorAll("div[uib-modal-window=modal-window]").forEach(i => i.remove());
+        document.querySelectorAll("div[uib-modal-backdrop=modal-backdrop]").forEach(i => i.remove());
     }
 
-    // The main loop, which runs as specified in the loopInterval
-    async function answerLoop() {
-        // As long as one of the modes is on, the loop runs
-        while ((semiTOGGLE === true) || (autoTOGGLE === true)) {
-            // If there's a question to be answered, it answers the questions, and if not, it corrects itself (woo ~learning~)
+    function findAnswer(question) {
+        let answer = fullDict[question];
+        if (answer === undefined) answer = fullDict[question.replace(",", ";")];
+        if (answer === undefined) answer = cutDict[cutString(question)];
+        return answer
+    }
+
+    async function correctAnswer(question) {
+        msg = msg + "Thought question: \n" + question + "\n";
+        for (let i = 0; i < 300; i++) {
+            if (cutString(document.querySelector("td#question-field").innerText) === cutString(question)) break;
+            await sleep(10);
+        }
+        msg = msg + "Modal question: " + document.querySelector("td#question-field").innerText + "\n";
+        let answer = cutString(document.querySelector("td#correct-answer-field").innerText);
+        document.querySelector("button#continue-button").disabled = false;
+        document.querySelector("button#continue-button").click();
+        msg = msg + "Modal cut answer: \n" + answer + "\n";
+        fullDict[question] = answer;
+    }
+
+    async function copyAnswer(answer) {
+        await navigator.clipboard.writeText(answer);
+    }
+
+    async function submitAnswer(answer) {
+        msg = "Thought answered: \n" + answer + "\n";
+        (await submitButton()).click();
+        document.querySelector("input#answer-text").value = answer;
+    }
+
+    async function answerLoop(answerFunc) {
+        let failCount = 0;
+        while (semiTOGGLE || autoTOGGLE) {
             try {
-                let question = cutString(document.querySelectorAll("#question-text")[0].innerText);
+                let question = document.querySelector("#question-text").innerText;
+                let answer = findAnswer(question);
+                if (answer === undefined) console.log("no answer found");
+                await answerFunc(answer);
 
-                if (question !== prevQuestion) {
-                    failCount = 0;
-                    prevQuestion = question;
-
-                    answerSub = fullDict[question];
-                    if (autoTOGGLE === true) {
-                        submitAnswer(answerSub);
-                    }
-
-                } else {
-                    if (document.querySelector("#correct-answer-field") != null && document.querySelector("#question-field") != null) {
-                        await sleep(1000);
-                        let correctQ = cutString(document.querySelector("#question-field").innerText);
-                        let correctA = cutString(document.querySelector("#correct-answer-field").innerText);
-                        fullDict[correctQ] = correctA;
-                        console.log('changed ' + correctQ + ' to ' + correctA);
-                        await sleep(100);
-                        document.querySelector("#continue-button").click();
-                    }
-
-                    failCount++;
-                    if (failCount > 3) {
-                        prevQuestion = null;
-                    }
+                if (document.querySelector("td#correct-answer-field") != null && document.querySelector("#question-field") != null) {
+                    await correctAnswer(question);
+                    console.log(msg);
+                    msg = "";
+                    deleteModals();
                 }
-
-                let keepRevisingButton = document.querySelector("#keep-revising");
-                if (keepRevisingButton != null) {
-                    keepRevisingButton.click();
-                }
-
+                failCount = 0;
             } catch (err) {
                 failCount++;
-                console.log(err)
-                if (failCount > 20) {
+                if (failCount > 30) {
                     semiTOGGLE = false;
                     autoTOGGLE = false;
-                    prevQuestion = null;
-                    console.log("An Error has occurred.");
                     console.log(err);
                     alert("Error, Auto-Answer Stopped.");
                 }
             }
-            await sleep(loopInterval);
+            await sleep(1);
         }
     }
 
-    // This adds an event listener which reacts appropriately based on the keyboard keys being clicked
-    document.addEventListener("keydown", (event) => {
-        // Opt/Alt + S
-        if ((event.altKey && event.keyCode === 83) || (event.altKey && event.key === 's')) {
-            if (autoTOGGLE === false) {
-                loopInterval = parseInt(prompt("How fast would you like to answer the questions (in milliseconds)"))
-                if (isNaN(loopInterval)) {
-                    alert("Cancelled Fully-Auto-Answer");
-                    loopInterval = 10;
-                    setToggle(false, false);
-                } else {
-                    alert("Starting Fully-Auto-Answer");
-                    setToggle(false, true);
-                    answerLoop();
-                }
-            } else if (autoTOGGLE === true) {
-                alert("Stopping Fully-Auto-Answer");
-                loopInterval = 10;
-                setToggle(false, false);
-            }
-        }
-        // Cmd/Ctrl
-        else if ((event.metaKey && semiTOGGLE === true) || (event.ctrlKey && semiTOGGLE === true)) {
-            navigator.clipboard.writeText(answerSub).then(function(arr) {}, function(arr) {
-                console.log(`Failed to Copy Answer: ${arr}`)
-            });
-        }
-        // Opt/Alt + A
-        else if ((event.altKey && event.keyCode === 65) || (event.altKey && event.key === 'a')) {
-            if (semiTOGGLE === false) {
-                alert("Starting Semi-Auto-Answer");
-                loopInterval = 10;
-                setToggle(true, false);
-                answerLoop();
-            } else if (semiTOGGLE === true) {
-                alert("Stopping Semi-Auto-Answer");
-                setToggle(false, false);
-            }
-        }
-        // Opt/Alt + R
-        else if ((event.altKey && event.keyCode === 82) || (event.altKey && event.key === 'r')) {
-            fullDict = mergeLists(wordList("targetLanguage"), wordList("baseLanguage"));
-            console.log(fullDict);
+    document.addEventListener("keydown", async (event) => {
+        if (event.altKey && event.key.toLowerCase() === "r") {
+            mergeLists(wordList("targetLanguage"), wordList("baseLanguage"));
+            console.log(fullDict, cutDict);
             alert("Word List Refreshed");
         }
+
+        if (event.altKey && event.key.toLowerCase() === "a") {
+            if (semiTOGGLE === false) {
+                autoTOGGLE = false;
+                semiTOGGLE = true;
+                await sleep(10);
+                alert("Starting Semi-Auto-Answer");
+                await answerLoop(copyAnswer);
+            } else {
+                alert("Stopping Semi-Auto-Answer");
+                semiTOGGLE = false;
+            }
+        }
+
+        if (event.altKey && event.key.toLowerCase() === "s") {
+            if (autoTOGGLE === false) {
+                semiTOGGLE = false;
+                autoTOGGLE = true;
+                await sleep(10);
+                alert("Starting Fully-Auto-Answer");
+                await answerLoop(submitAnswer);
+            } else {
+                alert("Stopping Fully-Auto-Answer");
+                autoTOGGLE = false;
+            }
+        }
     });
+    console.log("Education Perfected by KEN and Garv Loaded");
 })();
